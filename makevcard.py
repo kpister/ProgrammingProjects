156e6a2f6a3b106433a0eb366c11547eff25afdc
Kaiser Pister <pisterk@gmail.com>
2016-09-03 14:13:57 -0700

    convert excel file to vcard for contact importing

---
+++ makevcard.py
@@ -0,0 +1,16 @@
+
+
+ex = open("contacts.txt").read()
+loadcont = ex.split("\n")
+
+newFile = open("newContacts.txt", 'w')
+
+for contact in loadcont:
+    info = contact.split(",")
+    newFile.write("BEGIN:VCARD\nVERSION:4.0\nN:%s;%s;;;\nFN:%s\n" %(info[0],info[1], (info[0]+info[1])))
+    newFile.write("TEL;TYPE=work,voice;VALUE=uri:%s\n" %(info[2]))
+
+    if info[3].isalnum():
+        newFile.write("TEL;TYPE=work,voice;VALUE=uri:%s\n" %(info[3]))
+    newFile.write("END:VCARD\n")
+
