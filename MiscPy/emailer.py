ac0f63715a5917cb591ee27efbae99048486b5ee
Kaiser Pister <pisterk@gmail.com>
2017-05-25 12:43:24 -0700

    updated py files

--- MiscPy/emailer.py
+++ MiscPy/emailer.py
@@ -2,16 +2,19 @@ import smtplib
 
 from email.mime.text import MIMEText
 
+pw = open("pw.txt").readline()
+pw.strip()
+
 with open ('dummy.txt') as fp:
     msg = MIMEText(fp.read())
 
 msg['Subject'] = "Tester email"
 msg['From'] = "pisterk1@gmail.com"
-msg['To'] = "vip.pister@gmail.com"
+msg['To'] = "sanjeev96red@gmail.com"
 
 s = smtplib.SMTP('smtp.gmail.com')
 s.ehlo()
 s.starttls()
-s.login("pisterk1@gmail.com", "password")
+s.login("pisterk1@gmail.com", pw)
 s.send_message(msg)
 s.quit()
